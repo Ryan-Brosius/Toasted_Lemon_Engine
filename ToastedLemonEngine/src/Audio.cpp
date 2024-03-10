ba698168e86288b98f470cfe130447b0c394c0a7
RickyChen67 <Ricky.Chen6767@gmail.com>
2024-03-10 15:41:41 -0400

    Why ;-;

--- ToastedLemonEngine/src/Audio.cpp
+++ ToastedLemonEngine/src/Audio.cpp
@@ -6,21 +6,35 @@ Audio::Audio() {
 }
 
 Audio::~Audio() {
+	// Frees all the Sound objects
 	for (const auto& item : audioFiles) {
 		item.second->~Sound();
 	}
 }
 
 void Audio::init() {
+	// Initializes all Sound objects for all files in Assets/Audio
 	for (const auto& entry : std::filesystem::directory_iterator(audioPath)) {
 		audioFiles[entry.path().filename().string()] = new Sound(entry.path().string().c_str());
 	}
 }
 
-int Audio::PlayAudio(const char* fileName, int loop) {
-	return audioFiles[fileName]->PlaySound(loop);
+void Audio::Update() {
+	// Checks if there are files to loop and if there is, updates the loop Sound objects
+	if (!loopFiles.empty()) {
+		for (const auto& entry : loopFiles) {
+			audioFiles[entry]->Update();
+		}
+	}
+}
+
+void Audio::PlayAudio(const char* fileName, int loop) {
+	// Plays the audio for the given file name and sets a Sound to loop if flag - loop > 0
+	if (loop) loopFiles.insert(loopFiles.end(), fileName);
+	audioFiles[fileName]->PlaySound();
 }
 
 void Audio::StopAudio(const char* fileName) {
+	// Stops the audio for the given file name
 	audioFiles[fileName]->StopSound();
 }
--- ToastedLemonEngine/src/Sound.h
+++ ToastedLemonEngine/src/Sound.h
@@ -2,16 +2,25 @@
 #include "Game.h"
 #include <vector>
 
+extern Game* game;
+
 class Sound {
 public:
 	Sound(const char* filePath);
 	~Sound();
-	int PlaySound(int loop);
+	void Update();
+	void PlaySound();
 	void StopSound();
+	int GetPlayTime();
 
 private:
+	bool PlayTimeReached();
+
 	SDL_AudioDeviceID audioDevice;
 	SDL_AudioSpec wav_spec;
 	Uint32 wav_length;
 	Uint8* wav_buffer;
+
+	double playTime;  // The current timer - if looping is enabled
+	double audioLength;  // The loaded WAV file's length in seconds
 };
